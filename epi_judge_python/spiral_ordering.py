78e579e322d95acecbfe6a198fdd3b6ab95a79c8
Matthew Seah <matscottseah@gmail.com>
2021-11-29 20:54:27 -0800

    5.18 - O(n^2) time, O(n) space; traded brevity for clarity

--- epi_judge_python/spiral_ordering.py
+++ epi_judge_python/spiral_ordering.py
@@ -4,8 +4,35 @@ from test_framework import generic_test
 
 
 def matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    spiral = []
+    iMin = jMin = 0
+    iMax = jMax = len(square_matrix) - 1
+
+    iterations = (len(square_matrix) * 2) -1
+    step = 0
+
+    for i in range(iterations):
+        currentSpiral = []
+        if step % 4 == 0:
+            for j in range(jMin, jMax + 1):
+                currentSpiral.append(square_matrix[iMin][j])
+            iMin += 1
+        elif step % 4 == 1:
+            for i in range(iMin, iMax + 1):
+                currentSpiral.append(square_matrix[i][jMax])
+            jMax -= 1
+        elif step % 4 == 2:
+            for j in range(jMax, jMin - 1, -1):
+                currentSpiral.append(square_matrix[iMax][j])
+            iMax -= 1
+        else:
+            for i in range(iMax, iMin - 1, -1):
+                currentSpiral.append(square_matrix[i][jMin])
+            jMin += 1
+        spiral += currentSpiral
+        step += 1
+
+    return spiral
 
 
 if __name__ == '__main__':
