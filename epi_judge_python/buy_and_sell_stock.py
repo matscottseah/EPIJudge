bc5df9c51fafe951c7350bf5d8893ef3d2350f8f
Matthew Seah <matscottseah@gmail.com>
2021-11-29 18:13:15 -0800

    5.6 - O(n) time, O(1) space

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -4,8 +4,17 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_once(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    currentMax = 0.0
+    buy = sell = 0
+    while sell < len(prices):
+        diff = prices[sell] - prices[buy]
+        if diff < 0:
+            buy = sell
+        else:
+            currentMax = max(diff, currentMax)
+            sell += 1
+        
+    return currentMax
 
 
 if __name__ == '__main__':
