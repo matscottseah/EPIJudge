9b06e2d9c64ad8015329aab420ad42d59a8e1c7d
Matthew Seah <matscottseah@gmail.com>
2021-11-30 21:20:44 -0800

    6.2 - O(n), O(n) space

--- epi_judge_python/convert_base.py
+++ epi_judge_python/convert_base.py
@@ -1,9 +1,63 @@
 from test_framework import generic_test
 
+strToInt = {
+    '0': 0,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    'A': 10,
+    'B': 11,
+    'C': 12,
+    'D': 13,
+    'E': 14,
+    'F': 15
+}
+
+intToStr = {
+    0: '0',
+    1: '1',
+    2: '2',
+    3: '3',
+    4: '4',
+    5: '5',
+    6: '6',
+    7: '7',
+    8: '8',
+    9: '9',
+    10: 'A',
+    11: 'B',
+    12: 'C',
+    13: 'D',
+    14: 'E',
+    15: 'F'
+}
 
 def convert_base(num_as_string: str, b1: int, b2: int) -> str:
-    # TODO - you fill in here.
-    return ''
+    if num_as_string == '0':
+        return '0'
+
+    decimal = 0
+    negative = False
+    if num_as_string[0] == '-':
+        negative = True
+
+    for i in range(len(num_as_string)):
+        if num_as_string[len(num_as_string) - 1 - i] == '-':
+            break
+        decimal += (strToInt[num_as_string[len(num_as_string) - 1 - i]] * (b1 ** i))
+
+    result = []
+    while decimal:
+        result.append(intToStr[decimal % b2])
+        decimal //= b2
+
+    return ('-' if negative else '') + ''.join(reversed(result))
 
 
 if __name__ == '__main__':
