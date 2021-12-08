97821cf790e19215483db9a17db34bdbef2635d3
Matthew Seah <matscottseah@gmail.com>
2021-12-08 14:17:45 -0800

    8.6 - O(n) time, O(m) space where m = max number of nodes at level

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -5,8 +5,29 @@ from test_framework import generic_test
 
 
 def binary_tree_depth_order(tree: BinaryTreeNode) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    levels = []
+    if not tree:
+        return levels
+
+    Q = [tree]
+    levelCount = 1
+
+    while len(Q) != 0:
+        level = []
+        for _ in range(levelCount):
+            node = Q[0]
+            if node.left:
+                Q.append(node.left)
+            if node.right:
+                Q.append(node.right)
+
+            level.append(node.data)
+            Q.pop(0)
+
+        levels.append(level)
+        levelCount = len(Q)
+
+    return levels
 
 
 if __name__ == '__main__':
