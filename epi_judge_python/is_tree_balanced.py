23a522d38b352572e4eb9e96c1d7c725e677da8e
Matthew Seah <matscottseah@gmail.com>
2022-01-03 19:04:58 -0800

    complete 9.1 - O(n) time, O(h) space

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -1,10 +1,30 @@
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
+from collections import namedtuple
 
+nodeStats = namedtuple('nodeStats', 'balanced height')
 
 def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    def testBalance(node: BinaryTreeNode) -> nodeStats:
+        if not node:
+            return nodeStats(True, 0)
+        elif not node.left and not node.right:
+            return nodeStats(True, 1)
+        else:
+            leftStats = testBalance(node.left)
+            if not leftStats.balanced:
+                return leftStats
+
+            rightStats = testBalance(node.right)
+            if not rightStats.balanced:
+                return rightStats
+
+            subTreeBalanced = leftStats.balanced and rightStats.balanced and abs(leftStats.height - rightStats.height) <= 1
+            subTreeHeight = 1 + max(leftStats.height, rightStats.height)
+
+            return nodeStats(subTreeBalanced, subTreeHeight)
+
+    return testBalance(tree).balanced
 
 
 if __name__ == '__main__':
